ada689e2998b3c843a429e928a46b735943e4e46
Muthu Selvan <muthuselvanfxec@gmail.com>
2023-01-03 07:33:41 +0530

    Accessing instance variable

--- src/main/kotlin/oop/accessModifiers/AccessModifer.kt
+++ src/main/kotlin/oop/accessModifiers/AccessModifer.kt
@@ -7,11 +7,21 @@ fun main() {
 
     var emp2 = Employee("Muthu", false)
     println(emp2)
+
+    println(Demo().dummy)
 }
 
 class Employee(var firstName: String, var fullTime: Boolean = true) {
 
     override fun toString(): String {
         return "Employee [ First Name : ${firstName} FullTime ${fullTime} ]"
+    }
+}
+
+class Demo {
+    var dummy: String
+
+    constructor() {
+        dummy = "Dummy Value"
     }
 }
